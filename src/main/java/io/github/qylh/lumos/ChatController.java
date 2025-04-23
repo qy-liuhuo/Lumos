6376859701da7b8d201dca4a32e68351d4e0723c
mahaotian's debian <maht2001@163.com>
2025-04-23 16:24:18 +0800

    Added stream output

--- src/main/java/io/github/qylh/lumos/ChatController.java
+++ src/main/java/io/github/qylh/lumos/ChatController.java
@@ -6,15 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
 
     @Autowired
     private ChatService chatService;
+
     @GetMapping("/chat")
     public String chat(@RequestParam(value = "msg", defaultValue = "Hello") String msg) {
         return chatService.getResponse(msg);
+    }
+
+    @GetMapping("/chat/stream")
+    public Flux<String> getStreamResponse(@RequestParam(value = "msg", defaultValue = "Hello") String msg) {
+        return chatService.getStreamResponse(msg);
     }
 
 }
--- src/main/java/io/github/qylh/lumos/ChatService.java
+++ src/main/java/io/github/qylh/lumos/ChatService.java
@@ -2,9 +2,9 @@ package io.github.qylh.lumos;
 
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 @Service
 public class ChatService {
@@ -14,6 +14,10 @@ public class ChatService {
 
     public String getResponse(String msg){
         return chatClient.prompt().user(msg).call().content();
+    }
+
+    public Flux<String> getStreamResponse(String msg){
+        return chatClient.prompt().user(msg).stream().content();
     }
 
 }
